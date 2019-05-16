ff96d49d473b87301eabfe29ca507815972d02a4
Unknown <skryshtafovych@gmail.com>
2019-05-15 22:40:56 -0700

    Corrected some variable names for future ease

--- GarageDetectorActorRecorder.ino
+++ GarageDetectorActorRecorder.ino
@@ -3,41 +3,43 @@
 
   Often, you don't need to know the state of a digital input all the time, but
   you just need to know when the input changes from one state to another.
-  For example, you want to know when a button goes from OFF to ON. This is called
+  For example, you want to know when a Hall Sensor goes from OFF to ON. This is called
   state change detection, or edge detection.
 
   This example shows how to detect when a button or button changes from off to on
   and on to off.
 
   The circuit:
-  - pushbutton attached to pin 2 from +5V
-  - 10 kilohm resistor attached to pin 2 from ground
-  - LED attached from pin 13 to ground (or use the built-in LED on most
-    Arduino boards)
+  - hallSensor attached to pin 2 from +5V
+  - Relay attached from pin 5 to ground
 
-  created  27 Sep 2005
-  modified 30 Aug 2011
-  by Tom Igoe
+  created  27 Sep 2005 -by Tom Igoe
+  modified 30 Aug 2011 -by Tom Igoe
+  modified 09 May 2019 -by Stepan Kryshtafovych
 
-  This example code is in the public domain.
 
+
+  You Can find modified example on Github.
+  https://github.com/skryshtafovych/CustomHomeGarage
+
+  This was based on below example code is in the public domain.
   http://www.arduino.cc/en/Tutorial/ButtonStateChange
 */
 
 // this constant won't change:
-const int  buttonPin = 2;    // the pin that the pushbutton is attached to
-const int ledPin = 5;       // the pin that the LED is attached to
+const int  hallSensorPin = 2;    // the pin that the pushbutton is attached to
+const int relayPin = 5;       // the pin that the LED is attached to
 
 // Variables will change:
-int buttonPushCounter = 0;   // counter for the number of button presses
-int buttonState = 0;         // current state of the button
-int lastButtonState = 0;     // previous state of the button
+int hallSensorCounter = 0;   // counter for the number of button presses
+int hallSensorState = 0;         // current state of the button
+int lastHallSensorState = 0;     // previous state of the button
 
 void setup() {
   // initialize the button pin as a input:
-  pinMode(buttonPin, INPUT);
+  pinMode(hallSensorPin, INPUT);
   // initialize the LED as an output:
-  pinMode(ledPin, OUTPUT);
+  pinMode(relayPin, OUTPUT);
   // initialize serial communication:
   Serial.begin(9600);
 }
@@ -45,17 +47,17 @@ void setup() {
 
 void loop() {
   // read the pushbutton input pin:
-  buttonState = digitalRead(buttonPin);
+  hallSensorState = digitalRead(hallSensorPin);
 
-  // compare the buttonState to its previous state
-  if (buttonState != lastButtonState) {
+  // compare the hallSensorState to its previous state
+  if (hallSensorState != lastHallSensorState) {
     // if the state has changed, increment the counter
-    if (buttonState == HIGH) {
+    if (hallSensorState == HIGH) {
       // if the current state is HIGH then the button went from off to on:
-      buttonPushCounter++;
+      hallSensorCounter++;
       Serial.println("on");
       Serial.print("number of button pushes: ");
-      Serial.println(buttonPushCounter);
+      Serial.println(hallSensorCounter);
     } else {
       // if the current state is LOW then the button went from on to off:
       Serial.println("off");
@@ -64,16 +66,16 @@ void loop() {
     delay(50);
   }
   // save the current state as the last state, for next time through the loop
-  lastButtonState = buttonState;
+  lastHallSensorState = hallSensorState;
 
 
   // turns on the LED every four button pushes by checking the modulo of the
   // button push counter. the modulo function gives you the remainder of the
   // division of two numbers:
-  if (buttonPushCounter % 2 == 0) {
-    digitalWrite(ledPin, HIGH);
+  if (hallSensorCounter % 2 == 0) {
+    digitalWrite(relayPin, HIGH);
   } else {
-    digitalWrite(ledPin, LOW);
+    digitalWrite(relayPin, LOW);
   }
 
 }
